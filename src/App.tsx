59ffc5d41042f8b9f703395dd5a94bdb85780c5a
cankskrk <ardacankisakurek@gmail.com>
2025-02-14 18:18:49 +0100

    feat: setup react router and create pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,45 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  HomeLayout,
+  About,
+  Cocktail,
+  Error,
+  Landing,
+  Newsletter,
+} from "./pages";
 import "./App.css";
 
 const App = () => {
-  return <div>App</div>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomeLayout />,
+      children: [
+        {
+          path: "/about",
+          element: <About />,
+        },
+        {
+          path: "/cocktail",
+          element: <Cocktail />,
+        },
+        {
+          path: "/newsletter",
+          element: <Newsletter />,
+        },
+        {
+          path: "/landing",
+          element: <Landing />,
+        },
+        {
+          path: "/error",
+          element: <Error />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 };
 
 export default App;
