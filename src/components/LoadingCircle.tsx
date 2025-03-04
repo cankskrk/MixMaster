3dfea2acb4691d1d7dd03df2be5f85999f1f25f0
cankskrk <ardacankisakurek@gmail.com>
2025-03-04 08:43:41 +0100

    feat: implement global loading circle

---
+++ src/components/LoadingCircle.tsx
@@ -0,0 +1,9 @@
+const LoadingCircle = () => {
+  return (
+    <div className="flex items-center justify-center min-h-[50vh]">
+      <div className="w-16 h-16 border-8 border-gray-200 border-t-blue-500 rounded-full animate-spin"></div>
+    </div>
+  );
+};
+
+export default LoadingCircle;
--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -1,11 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import LoadingCircle from "../components/LoadingCircle";
 
 const HomeLayout = () => {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
   return (
     <div>
       <Navbar />
-      <section className="bg-gray-100 min-h-screen p-10">
+      <section className="bg-gray-100 min-h-screen flex flex-col items-center p-10">
+        {isLoading && <LoadingCircle />}
         <Outlet />
       </section>
     </div>
